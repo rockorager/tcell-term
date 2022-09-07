05b8a155aef4324fa26bfd618d773ae771e9ad14
Tim Culverhouse <tim@timculverhouse.com>
2022-09-07 08:52:42 -0500

    chore: remove unused code
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- termutil/cell.go
+++ termutil/cell.go
@@ -18,42 +18,6 @@ func (cell *Cell) Style() tcell.Style {
 	return cell.attr
 }
 
-/*
-func (cell *Cell) Fg() tcell.Color {
-	if cell.attr.inverse {
-		return cell.attr.bgColour
-	}
-	return cell.attr.fgColour
-}
-
-func (cell *Cell) Bold() bool {
-	return cell.attr.bold
-}
-
-func (cell *Cell) Dim() bool {
-	return cell.attr.dim
-}
-
-func (cell *Cell) Italic() bool {
-	return cell.attr.italic
-}
-
-func (cell *Cell) Underline() bool {
-	return cell.attr.underline
-}
-
-func (cell *Cell) Strikethrough() bool {
-	return cell.attr.strikethrough
-}
-
-func (cell *Cell) Bg() tcell.Color {
-	if cell.attr.inverse {
-		return cell.attr.fgColour
-	}
-	return cell.attr.bgColour
-}
-*/
-
 func (cell *Cell) Dirty() bool {
 	return cell.dirty
 }
