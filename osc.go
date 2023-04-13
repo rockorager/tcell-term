9b881ee510901902e5861878a3e429493eef0795
Tim Culverhouse <tim@timculverhouse.com>
2023-04-13 10:25:39 -0500

    osc: add OSC 8 parsing tests
    Add OSC8 parsing tests. Refactor slightly to make OSC8 parsing more
    testable
    
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- osc.go
+++ osc.go
@@ -17,22 +17,30 @@ func (vt *VT) osc(data string) {
 		}
 		vt.postEvent(ev)
 	case "8":
-		// OSC 8 ; params ; url ST
-		// params: key1=value1:key2=value2
-		params, url, found := strings.Cut(val, ";")
-		if !found {
-			return
-		}
-		for _, param := range strings.Split(params, ":") {
-			key, val, found := strings.Cut(param, "=")
-			if !found {
-				continue
-			}
-			switch key {
-			case "id":
-				vt.cursor.attrs = vt.cursor.attrs.UrlId(val)
-			}
-		}
-		vt.cursor.attrs = vt.cursor.attrs.Url(url)
+		url, id := osc8(val)
+		vt.cursor.attrs.Url(url)
+		vt.cursor.attrs.UrlId(id)
 	}
+}
+
+// parses an osc8 payload into the URL and optional ID
+func osc8(val string) (string, string) {
+	// OSC 8 ; params ; url ST
+	// params: key1=value1:key2=value2
+	var id string
+	params, url, found := strings.Cut(val, ";")
+	if !found {
+		return "", ""
+	}
+	for _, param := range strings.Split(params, ":") {
+		key, val, found := strings.Cut(param, "=")
+		if !found {
+			continue
+		}
+		switch key {
+		case "id":
+			id = val
+		}
+	}
+	return url, id
 }
--- osc_test.go
+++ osc_test.go
@@ -1,59 +1,75 @@
 package tcellterm
 
-// import (
-// 	"strings"
-// 	"testing"
-//
-// 	"github.com/stretchr/testify/assert"
-// )
-//
-// func TestParseOSC8(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		input    string
-// 		expected string
-// 	}{
-// 		{
-// 			name:     "no semicolon in URI",
-// 			input:    "8;;https://example.com",
-// 			expected: "https://example.com",
-// 		},
-// 		{
-// 			name:     "no semicolon in URI, with id",
-// 			input:    "8;id=hello;https://example.com",
-// 			expected: "https://example.com",
-// 		},
-// 		{
-// 			name:     "semicolon in URI",
-// 			input:    "8;;https://example.com/semi;colon",
-// 			expected: "https://example.com/semi;colon",
-// 		},
-// 		{
-// 			name:     "multiple semicolons in URI",
-// 			input:    "8;;https://example.com/s;e;m;i;colon",
-// 			expected: "https://example.com/s;e;m;i;colon",
-// 		},
-// 		{
-// 			name:     "semicolon in URI, with id",
-// 			input:    "8;id=hello;https://example.com/semi;colon",
-// 			expected: "https://example.com/semi;colon",
-// 		},
-// 		{
-// 			name:     "terminating sequence",
-// 			input:    "8;;",
-// 			expected: "",
-// 		},
-// 		{
-// 			name:     "terminating sequence with id",
-// 			input:    "8;id=hello;",
-// 			expected: "",
-// 		},
-// 	}
-//
-// 	for _, test := range tests {
-// 		t.Run(test.name, func(t *testing.T) {
-// 			uri := parseOSC8(strings.Split(test.input, ";"))
-// 			assert.Equal(t, test.expected, uri)
-// 		})
-// 	}
-// }
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseOSC8(t *testing.T) {
+	tests := []struct {
+		name       string
+		input      string
+		expected   string
+		expectedID string
+	}{
+		{
+			name:       "no semicolon in URI",
+			input:      "8;;https://example.com",
+			expected:   "https://example.com",
+			expectedID: "",
+		},
+		{
+			name:       "no semicolon in URI, with id",
+			input:      "8;id=hello;https://example.com",
+			expected:   "https://example.com",
+			expectedID: "hello",
+		},
+		{
+			name:       "semicolon in URI",
+			input:      "8;;https://example.com/semi;colon",
+			expected:   "https://example.com/semi;colon",
+			expectedID: "",
+		},
+		{
+			name:       "multiple semicolons in URI",
+			input:      "8;;https://example.com/s;e;m;i;colon",
+			expected:   "https://example.com/s;e;m;i;colon",
+			expectedID: "",
+		},
+		{
+			name:       "semicolon in URI, with id",
+			input:      "8;id=hello;https://example.com/semi;colon",
+			expected:   "https://example.com/semi;colon",
+			expectedID: "hello",
+		},
+		{
+			name:       "terminating sequence",
+			input:      "8;;",
+			expected:   "",
+			expectedID: "",
+		},
+		{
+			name:       "terminating sequence with id",
+			input:      "8;id=hello;",
+			expected:   "",
+			expectedID: "hello",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Simulate vt.osc
+			selector, val, found := strings.Cut(test.input, ";")
+			if !found {
+				return
+			}
+			assert.Equal(t, "8", selector)
+			// parse the result
+			url, id := osc8(val)
+			assert.Equal(t, test.expected, url)
+			assert.Equal(t, test.expectedID, id)
+		})
+	}
+}
